e9cbaa9a3feba201499f5c817bd199301594bfec
SinWolves <fadjardalimunthe45@gmail.com>
2024-10-23 18:05:02 +0700

    tambah banyak lh pokoknya

--- script.js
+++ script.js
@@ -1,19 +1,286 @@
-//API yang digunakan https://api.discogs.com/, https://musicbrainz.org/ws/2/
+let songs1 = document.getElementById("songs1");
+let songs2 = document.getElementById("songs2");
+let songs3 = document.getElementById("songs3");
+let songs4 = document.getElementById("songs4");
+let songs5 = document.getElementById("songs5");
+let songs6 = document.getElementById("songs6");
+let songs7 = document.getElementById("songs7");
+let songs8 = document.getElementById("songs8");
+let songs9 = document.getElementById("songs9");
+let songs10 = document.getElementById("songs10");
+let songs11 = document.getElementById("songs11");
+let songs12 = document.getElementById("songs12");
+let albums1 = document.getElementById("albums1");
+let albums2 = document.getElementById("albums2");
+let albums3 = document.getElementById("albums3");
+let albums4 = document.getElementById("albums4");
+let albums5 = document.getElementById("albums5");
+let albums6 = document.getElementById("albums6");
+let artists1 = document.getElementById("artists1");
+let artists2 = document.getElementById("artists2");
+let artists3 = document.getElementById("artists3");
+let artists4 = document.getElementById("artists4");
+let artists5 = document.getElementById("artists5");
+let artists6 = document.getElementById("artists6");
+let songsPopup1 = document.getElementById("songsPopup1");
+let songsPopup2 = document.getElementById("songsPopup2");
+let songsPopup3 = document.getElementById("songsPopup3");
+let songsPopup4 = document.getElementById("songsPopup4");
+let songsPopup5 = document.getElementById("songsPopup5");
+let songsPopup6 = document.getElementById("songsPopup6");
+let songsPopup7 = document.getElementById("songsPopup7");
+let songsPopup8 = document.getElementById("songsPopup8");
+let songsPopup9 = document.getElementById("songsPopup9");
+let songsPopup10 = document.getElementById("songsPopup10");
+let songsPopup11 = document.getElementById("songsPopup11");
+let songsPopup12 = document.getElementById("songsPopup12");
+let albumsPopup1 = document.getElementById("albumsPopup1");
+let albumsPopup2 = document.getElementById("albumsPopup2");
+let albumsPopup3 = document.getElementById("albumsPopup3");
+let albumsPopup4 = document.getElementById("albumsPopup4");
+let albumsPopup5 = document.getElementById("albumsPopup5");
+let albumsPopup6 = document.getElementById("albumsPopup6");
+let artistsPopup1 = document.getElementById("artistsPopup1");
+let artistsPopup2 = document.getElementById("artistsPopup2");
+let artistsPopup3 = document.getElementById("artistsPopup3");
+let artistsPopup4 = document.getElementById("artistsPopup4");
+let artistsPopup5 = document.getElementById("artistsPopup5");
+let artistsPopup6 = document.getElementById("artistsPopup6");
 
-fetchData();
+songs1.addEventListener("click", () => {
 
-async function fetchData() {
-    try{
-        const response = await fetch("https://musicbrainz.org/ws/2/artist/8264722b-df00-467a-858e-5c97cda169c9?inc=aliases&fmt=json");
+    songsPopup1.style.display = 'block';  
 
-        if (!response.ok) {
-            throw new Error("Data cannot be fetch");
-        }
+    document.querySelector('.close1').addEventListener('click', () => {
+        document.getElementById('songsPopup1').style.display = 'none';
+    });
 
-        const data = await response.json();
-        console.log(data);
-    }   
-    catch(error){
-        console.error(error);
-    }
-}
+});
+
+songs2.addEventListener("click", () => {
+
+    songsPopup2.style.display = 'block';  
+
+    document.querySelector('.close2').addEventListener('click', () => {
+        document.getElementById('songsPopup2').style.display = 'none';
+    });
+
+});
+
+songs3.addEventListener("click", () => {
+
+    songsPopup3.style.display = 'block';  
+
+    document.querySelector('.close3').addEventListener('click', () => {
+        document.getElementById('songsPopup3').style.display = 'none';
+    });
+
+});
+
+songs4.addEventListener("click", () => {
+
+    songsPopup4.style.display = 'block';  
+
+    document.querySelector('.close4').addEventListener('click', () => {
+        document.getElementById('songsPopup4').style.display = 'none';
+    });
+
+});
+
+songs5.addEventListener("click", () => {
+
+    songsPopup5.style.display = 'block';  
+
+    document.querySelector('.close5').addEventListener('click', () => {
+        document.getElementById('songsPopup5').style.display = 'none';
+    });
+
+});
+
+songs6.addEventListener("click", () => {
+
+    songsPopup6.style.display = 'block';  
+
+    document.querySelector('.close6').addEventListener('click', () => {
+        document.getElementById('songsPopup6').style.display = 'none';
+    });
+
+});
+
+songs7.addEventListener("click", () => {
+
+    songsPopup7.style.display = 'block';  
+
+    document.querySelector('.close7').addEventListener('click', () => {
+        document.getElementById('songsPopup7').style.display = 'none';
+    });
+
+});
+
+songs8.addEventListener("click", () => {
+
+    songsPopup8.style.display = 'block';  
+
+    document.querySelector('.close8').addEventListener('click', () => {
+        document.getElementById('songsPopup8').style.display = 'none';
+    });
+
+});
+
+songs9.addEventListener("click", () => {
+
+    songsPopup9.style.display = 'block';  
+
+    document.querySelector('.close9').addEventListener('click', () => {
+        document.getElementById('songsPopup9').style.display = 'none';
+    });
+
+});
+
+songs10.addEventListener("click", () => {
+
+    songsPopup10.style.display = 'block';  
+
+    document.querySelector('.close10').addEventListener('click', () => {
+        document.getElementById('songsPopup10').style.display = 'none';
+    });
+
+});
+
+songs11.addEventListener("click", () => {
+
+    songsPopup11.style.display = 'block';  
+
+    document.querySelector('.close11').addEventListener('click', () => {
+        document.getElementById('songsPopup11').style.display = 'none';
+    });
+
+});
+
+songs12.addEventListener("click", () => {
+
+    songsPopup12.style.display = 'block';  
+
+    document.querySelector('.close12').addEventListener('click', () => {
+        document.getElementById('songsPopup12').style.display = 'none';
+    });
+
+});
+
+albums1.addEventListener("click", () => {
+
+    albumsPopup1.style.display = 'block';  
+
+    document.querySelector('.close13').addEventListener('click', () => {
+        document.getElementById('albumsPopup1').style.display = 'none';
+    });
+
+});
+
+albums2.addEventListener("click", () => {
+
+    albumsPopup2.style.display = 'block';  
+
+    document.querySelector('.close14').addEventListener('click', () => {
+        document.getElementById('albumsPopup2').style.display = 'none';
+    });
+
+});
+
+albums3.addEventListener("click", () => {
+
+    albumsPopup3.style.display = 'block';  
+
+    document.querySelector('.close15').addEventListener('click', () => {
+        document.getElementById('albumsPopup3').style.display = 'none';
+    });
+
+});
+
+albums4.addEventListener("click", () => {
+
+    albumsPopup4.style.display = 'block';  
+
+    document.querySelector('.close16').addEventListener('click', () => {
+        document.getElementById('albumsPopup4').style.display = 'none';
+    });
+
+});
+
+albums5.addEventListener("click", () => {
+
+    albumsPopup5.style.display = 'block';  
+
+    document.querySelector('.close17').addEventListener('click', () => {
+        document.getElementById('albumsPopup5').style.display = 'none';
+    });
+
+});
+
+albums6.addEventListener("click", () => {
+
+    albumsPopup6.style.display = 'block';  
+
+    document.querySelector('.close18').addEventListener('click', () => {
+        document.getElementById('albumsPopup6').style.display = 'none';
+    });
+
+});
+
+artists1.addEventListener("click", () => {
+
+    artistsPopup1.style.display = 'block';  
+
+    document.querySelector('.close19').addEventListener('click', () => {
+        document.getElementById('artistsPopup1').style.display = 'none';
+    });
+
+});
+
+artists2.addEventListener("click", () => {
+
+    artistsPopup2.style.display = 'block';  
+
+    document.querySelector('.close20').addEventListener('click', () => {
+        document.getElementById('artistsPopup2').style.display = 'none';
+    });
+
+});
+
+artists3.addEventListener("click", () => {
+
+    artistsPopup3.style.display = 'block';  
+
+    document.querySelector('.close21').addEventListener('click', () => {
+        document.getElementById('artistsPopup3').style.display = 'none';
+    });
+
+});
+
+artists4.addEventListener("click", () => {
+
+    artistsPopup4.style.display = 'block';  
+
+    document.querySelector('.close22').addEventListener('click', () => {
+        document.getElementById('artistsPopup4').style.display = 'none';
+    });
+});
+
+artists5.addEventListener("click", () => {
+
+    artistsPopup5.style.display = 'block';  
+
+    document.querySelector('.close23').addEventListener('click', () => {
+        document.getElementById('artistsPopup5').style.display = 'none';
+    });
+});
+
+artists6.addEventListener("click", () => {
+
+    artistsPopup6.style.display = 'block';  
+
+    document.querySelector('.close24').addEventListener('click', () => {
+        document.getElementById('artistsPopup6').style.display = 'none';
+    });
+
+});
